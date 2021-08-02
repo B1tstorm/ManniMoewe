5d3971aac2409a6c40acea38cc283178882261ce
Marvin <marvin.winterhoff@student.fh-kiel.de>
2021-08-02 10:31:39 +0200

    ATFB-20 Funktion "multiplizieren" in public static geändert.

--- src/de/fhkiel/aem/Main.java
+++ src/de/fhkiel/aem/Main.java
@@ -3,20 +3,12 @@ package de.fhkiel.aem;
 public class Main {
 
     public static void main(String[] args) {
+
         System.out.println("hallo");
     }
 
-    public static double multiplizieren(double zahlEins, double zahlZwei){
+    public static double multiplizieren(double zahlEins, double zahlZwei) {
         return zahlEins * zahlZwei;
-
-    private static double subtraktion(double _minuend, double _subtrahend) {
-        return (_minuend - _subtrahend);
-
     }
 }
 
-    private static double addition(double _summand_1, double _summand_2) {
-        return (_summand_1 + _summand_2);
-
-    }
-}

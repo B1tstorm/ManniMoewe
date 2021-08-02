9dcb5df8b3cb542c7d304acd3d6ca7708ad66223
Marvin <marvin.winterhoff@student.fh-kiel.de>
2021-08-02 09:54:59 +0200

    ATFB-20 Funktion "multiplizieren" in public static geändert.

--- src/de/fhkiel/aem/Main.java
+++ src/de/fhkiel/aem/Main.java
@@ -6,7 +6,7 @@ public class Main {
         System.out.println("hallo");
     }
 
-    double multiplizieren(double zahlEins, double zahlZwei){
+    public static double multiplizieren(double zahlEins, double zahlZwei){
         return zahlEins * zahlZwei;
     }
 }

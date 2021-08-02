1d04dfd901a0a92ac37aecbb40877dc8e88773c3
anas.arodake <anas.arodake@student.fh-kiel.de>
2021-08-02 09:55:44 +0200

    ATFB-23 Methode zum Dividieren ist geliefert

--- src/de/fhkiel/aem/Main.java
+++ src/de/fhkiel/aem/Main.java
@@ -5,4 +5,11 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hallo");
     }
+
+    //berechnet die Division zweier Zahlen und gibt das Ergebniss zurück als double
+    public double divisoin (double first, double second){
+        return first/second;
+    }
+
+
 }

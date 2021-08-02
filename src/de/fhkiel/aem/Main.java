482a6c004a2c2ed7ca6ac06a692a72eec62c6857
Kai Harmsen <kai.t.harmsen@student.fh-kiel.de>
2021-08-02 10:07:44 +0200

    ATFB-18 Addition hinzugefügt

--- src/de/fhkiel/aem/Main.java
+++ src/de/fhkiel/aem/Main.java
@@ -8,6 +8,11 @@ public class Main {
     private static double subtraktion(double _minuend, double _subtrahend) {
         return (_minuend - _subtrahend);
     }
+
+    private static double addition(double _summand_1, double _summand_2) {
+        return (_summand_1 + _summand_2);
+
+    }
 }
 
 

f00cf2ae5c7ee5f09ed024d17a05ec1b771962e5
Josh <aCay7Jee4KohruSh>
2021-08-02 10:03:25 +0200

    ATFB-22 Add input parsing

--- src/de/fhkiel/aem/Main.java
+++ src/de/fhkiel/aem/Main.java
@@ -1,8 +1,48 @@
 package de.fhkiel.aem;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("hallo");
     }
+
+
+    private static double parseInput(String input) {
+        Pattern pattern = Pattern.compile(
+                "(?<numone>-?[0-9]+[.,]?[0-9]+?\\s?)(?<sign>[+\\-*/])\\s?(?<numtwo>-?[0-9]+[.,]?[0-9]+?)");
+        Matcher matcher = pattern.matcher(input);
+
+        if (!matcher.matches()) {
+            throw new RuntimeException("Die Rechnung konnte nicht erkannt werden!");
+        }
+
+        String numOne = matcher.group("numone");
+        String numTwo = matcher.group("numtwo");
+
+        String sign = matcher.group("sign");
+        double result;
+
+        switch (sign) {
+            case "-":
+                result = 0;
+                break;
+            case "+":
+                result = 0;
+                break;
+            case "*":
+                result = 0;
+                break;
+            case "/":
+                result = 0;
+                break;
+            default:
+                throw new RuntimeException("Die Rechnung konnte nicht erkannt werden!");
+        }
+
+        return result;
+    }
+
 }

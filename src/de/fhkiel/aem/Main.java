71892519d71ad2fa32487b59e81f342026add3be
Paul Rieck <paul.rieck@student.fh-kiel.de>
2021-08-02 09:55:12 +0200

    ATFB-19 Subtraktion von zwei double Werten funktioniert und wird zurück gegeben. Struktur wurde nochmal verbessert

--- src/de/fhkiel/aem/Main.java
+++ src/de/fhkiel/aem/Main.java
@@ -5,7 +5,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hallo");
     }
-    double subtrahieren(double zahl_eins, double zahl_zwei){return zahl_eins - zahl_2;}
+    private static double subtraktion(double _minuend, double _subtrahend) {
+        return (_minuend - _subtrahend);
+    }
 }
 
 

c21c887d0f481b4cfa68ec821449d33339112f89
Marvin <marvin.winterhoff@student.fh-kiel.de>
2021-08-02 09:50:08 +0200

    ATFB-20 Funktion "multiplizieren" erstellt

--- src/de/fhkiel/aem/Main.java
+++ src/de/fhkiel/aem/Main.java
@@ -5,4 +5,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hallo");
     }
+
+    double multiplizieren(double zahlEins, double zahlZwei){
+        return zahlEins * zahlZwei;
+    }
 }

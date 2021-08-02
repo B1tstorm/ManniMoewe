974091df9c34e7a08e75378866ba7040bc2eb1b7
Jonas Hermann Becker <jonas.h.becker@student.fh-kiel.de>
2021-08-02 10:04:41 +0200

    ATFB-21
    Main Methode zum Einlesen der Nutzereingabe.

--- src/de/fhkiel/aem/Main.java
+++ src/de/fhkiel/aem/Main.java
@@ -1,8 +1,24 @@
 package de.fhkiel.aem;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hallo");
+        String input;
+        Scanner scanner = new Scanner(System.in);
+
+        do {
+            System.out.println("Gib eine Rechnung (Bsp. X+X) ein oder \"-help\".");
+            input = scanner.next();
+            if(input.equals("-help")) {
+                Utility.help();
+            }
+
+        } while (!input.equals("off"));
+
+        scanner.close();
+        System.out.println("Taschenrechner beendet.");
     }
 }
+

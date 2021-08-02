39d7828c561377b12d3329269d934618b68e1bdc
Marvin Winterhoff <marvin.winterhoff@student.fh-kiel.de>
2021-08-02 08:35:58 +0000

    Update src/de/fhkiel/aem/Main.java

--- src/de/fhkiel/aem/Main.java
+++ src/de/fhkiel/aem/Main.java
@@ -7,8 +7,16 @@ public class Main {
         System.out.println("hallo");
     }
 
+    private static double subtraktion(double _minuend, double _subtrahend) {
+        return (_minuend - _subtrahend);
+    }
+
+    private static double addition(double _summand_1, double _summand_2) {
+        return (_summand_1 + _summand_2);
+
     public static double multiplizieren(double zahlEins, double zahlZwei) {
         return zahlEins * zahlZwei;
     }
 }
+
 

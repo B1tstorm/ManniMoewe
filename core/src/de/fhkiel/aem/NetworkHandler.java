b5764708e30396da25d9884955303d20a1475a32
Josh <aCay7Jee4KohruSh>
2021-08-09 13:58:01 +0200

    ATFB-111 Add JSON read

--- core/src/de/fhkiel/aem/NetworkHandler.java
+++ core/src/de/fhkiel/aem/NetworkHandler.java
@@ -1,5 +1,8 @@
 package de.fhkiel.aem;
 
+import de.fhkiel.aem.model.HighscoreEntry;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import de.fhkiel.aem.model.Highscore;
 
 public class NetworkHandler {
@@ -8,9 +11,28 @@ public class NetworkHandler {
 
 	}
 
-	private Highscore readJson() {
+	private Highscore readJson(String jsonString) {
+		Highscore highscore = new Highscore();
+		JSONObject jsonObject = new JSONObject(jsonString);
+		JSONArray easyArray = jsonObject.getJSONArray("easy");
+		JSONArray mediumArray = jsonObject.getJSONArray("medium");
+		JSONArray hardArray = jsonObject.getJSONArray("hard");
 
-		return null;
+		for(Object obj: easyArray) {
+			JSONObject jObj = (JSONObject) obj;
+			highscore.addHighscore(1, new HighscoreEntry(jObj.getString("name"), jObj.getInt("highscore")));
+		}
+
+		for(Object obj: mediumArray) {
+			JSONObject jObj = (JSONObject) obj;
+			highscore.addHighscore(2, new HighscoreEntry(jObj.getString("name"), jObj.getInt("highscore")));
+		}
+
+		for(Object obj: hardArray) {
+			JSONObject jObj = (JSONObject) obj;
+			highscore.addHighscore(3, new HighscoreEntry(jObj.getString("name"), jObj.getInt("highscore")));
+		}
+		return highscore;
 	}
 
 	public void sendToServer(Highscore highscore) {
@@ -19,6 +41,6 @@ public class NetworkHandler {
 
 	public Highscore getFromServer() {
 
-		return new Highscore();
+		return readJson("");
 	}
 }

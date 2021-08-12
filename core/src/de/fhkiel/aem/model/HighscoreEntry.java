01bf706a149ced76ab9b42d1276a80e2b0f6896f
Josh <aCay7Jee4KohruSh>
2021-08-12 16:32:52 +0200

    ATFB-192 Changed NetworkHandler JSON

--- core/src/de/fhkiel/aem/model/HighscoreEntry.java
+++ core/src/de/fhkiel/aem/model/HighscoreEntry.java
@@ -4,8 +4,11 @@ package de.fhkiel.aem.model;
  * HighscoreEntry with name and highscore.
  */
 public class HighscoreEntry {
-	public final String name;
-	public final int highscore;
+	public String name;
+	public int highscore;
+
+	// Empty Constructor for JSON serialization
+	public HighscoreEntry() { }
 
 	public HighscoreEntry(final String name, final int highscore) {
 		this.highscore = highscore;

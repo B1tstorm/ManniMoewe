22441e25fd3474415220cd3201a7dfde88a4fefa
Marvin <marvin.winterhoff@student.fh-kiel.de>
2021-08-12 15:54:17 +0200

    ATFB-167 Button Position angepasst

--- core/src/de/fhkiel/aem/HelpScreen.java
+++ core/src/de/fhkiel/aem/HelpScreen.java
@@ -65,38 +65,46 @@ public class HelpScreen implements Screen {
 
         table.background(new TextureRegionDrawable(new Texture(Configuration.table_backgroundImg)));
 
-        table.add(helpLabel).height(100).colspan(3).center();
+        table.add(helpLabel).height(100).colspan(4).center();
         table.row();
+        table.add().width(20);
         table.add(itemLabel).minWidth(300);
         table.add(descriptionLabel).expand().left().colspan(2);
 
         table.row();
+        table.add().width(20);
         table.add(new Image(new Texture(Configuration.manny_straightImg))).maxWidth(100).left().padRight(20);
         table.add(new Label("Manni: Die geilste Moewe der Welt.", labelStyle)).left().colspan(2);
 
         table.row();
+        table.add().width(20);
         table.add(new Image(new Texture(Configuration.barrierhelpImg))).maxWidth(100).left().padRight(20);
         table.add(new Label("Hindernis: Bei Beruehrung stribt Manni.", labelStyle)).left().colspan(2);
 
         table.row();
+        table.add().width(20);
         table.add(new Image(new Texture(Configuration.pommesImg))).maxWidth(100).maxHeight(100).left().padRight(20);
         table.add(new Label("Item-Pommes: Wenn du drei Pommes gesammelt hast kannst du von einem\n" +
                 "Hindernis getroffen werden ohne zu sterben.", labelStyle)).left().colspan(2);
 
         table.row();
+        table.add().width(20);
         table.add(new Image(new Texture(Configuration.item_miniImg))).maxWidth(100).left().padRight(20);
         table.add(new Label("Item-Lupe: Verkleinert Manni fuer eine kurze Zeit.", labelStyle)).left().colspan(2);
 
         table.row();
+        table.add().width(20);
         table.add(new Image(new Texture(Configuration.item_multipyImg))).maxWidth(100).left().padRight(20);
         table.add(new Label("Item-Multiplikator: Die erhaltenen Punkte werden fuer eine " +
                 "kurze Zeit verdoppelt", labelStyle)).left().colspan(2);
 
         table.row();
+        table.add().width(20);
         table.add(new Image(new Texture(Configuration.item_slowmoImg))).maxWidth(100).left().padRight(20);
         table.add(new Label("Item-Schnecke: Manni wird verlangsamt.", labelStyle)).left().colspan(2);
 
         table.row();
+        table.add().width(20);
         table.add(new Image(new Texture(Configuration.reduceScoreImg))).maxWidth(100).left().padRight(20);
         table.add(new Label("Item-Minus: Manni verliert einen Teil seiner Punkte.", labelStyle)).left().colspan(2);
 
@@ -105,15 +113,15 @@ public class HelpScreen implements Screen {
         buttonTable.padLeft(padding);
         buttonTable.add().expand().colspan(3);
         buttonTable.row();
-        buttonTable.add().fillX().minWidth((Gdx.graphics.getWidth() / 3) - (padding * 8));
-        buttonTable.add().fillX().minWidth((Gdx.graphics.getWidth() / 3) - (padding * 8));
+        buttonTable.add().fillX().minWidth((Gdx.graphics.getWidth() / 3) - (padding * 12));
+        buttonTable.add().fillX().minWidth((Gdx.graphics.getWidth() / 3) - (padding * 12));
         buttonTable.add(ButtonFactory.CreateImageButton(Configuration.back, false,
                 () -> {
                     game.setScreen(new StartScreen(game));
                     dispose();
                 })).bottom().center();
 
-        table.add(buttonTable).colspan(3).fillX();
+        table.add(buttonTable).colspan(4).fillX();
 
         stage.addActor(table);
     }

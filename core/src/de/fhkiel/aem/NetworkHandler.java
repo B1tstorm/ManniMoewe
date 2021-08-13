01c8414925b7085d8d2207c0d4db08a6002b2cfd
kamil.o.gorczyca <kamil.o.gorczyca@student.fh-kiel.de>
2021-08-13 13:02:45 +0200

    NetworkHandler ausgebaut, Highscore nicht nutzbar.

--- core/src/de/fhkiel/aem/NetworkHandler.java
+++ core/src/de/fhkiel/aem/NetworkHandler.java
@@ -4,10 +4,7 @@ import com.badlogic.gdx.utils.Json;
 import com.badlogic.gdx.utils.JsonWriter;
 import de.fhkiel.aem.model.Highscore;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
@@ -18,12 +15,12 @@ import java.nio.charset.StandardCharsets;
  */
 public class NetworkHandler {
 
-	private final Json json;
+//	private final Json json;
 	private final String serverUrl = "http://62.75.175.207:3000/api/highscore";
 
 	public NetworkHandler() {
-		json = new Json();
-		json.setOutputType(JsonWriter.OutputType.json);
+//		json = new Json();
+//		json.setOutputType(JsonWriter.OutputType.json);
 	}
 
 	/**
@@ -31,28 +28,16 @@ public class NetworkHandler {
 	 * @param highscore Highscore to send
 	 */
 	public void sendToServer(Highscore highscore) {
-		try {
-			URL url = new URL(serverUrl);
-			URLConnection con = url.openConnection();
-			HttpURLConnection http = (HttpURLConnection)con;
-			http.setRequestMethod("POST");
-			http.setDoOutput(true);
-
-			byte[] out = json.toJson(highscore).getBytes(StandardCharsets.UTF_8);
-			int length = out.length;
-
-			http.setFixedLengthStreamingMode(length);
-			http.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
-			http.connect();
-			try(OutputStream os = http.getOutputStream()) {
-				os.write(out);
-			}
-			System.out.println(json.toJson(highscore));
-			System.out.println("Send JSON to Server!");
-		} catch (Exception e) {
-			System.out.println("Outer Catch!");
-			System.out.println(e.getMessage());
-		}
+//		try {
+//			try(FileWriter writer = new FileWriter("highscore.json")){
+//				writer.write(json.toJson(highscore));
+//			}
+//			System.out.println(json.toJson(highscore));
+//
+//		} catch (Exception e) {
+//			System.out.println("Outer Catch!");
+//			System.out.println(e.getMessage());
+//		}
 	}
 
 	/**
@@ -60,20 +45,19 @@ public class NetworkHandler {
 	 * @return Received Highscore
 	 */
 	public Highscore getFromServer() {
-		String jsonText = "";
-		Highscore highscore;
-		try {
-			InputStream stream = new URL(serverUrl).openStream();
-			try(BufferedReader reader = new BufferedReader(new InputStreamReader(stream, StandardCharsets.UTF_8))) {
-				System.out.println("Got JSON from Server!");
-				System.out.println(jsonText);
-				highscore = json.fromJson(Highscore.class, reader);
-			}
-		} catch (Exception e) {
-			System.out.println("Outer Catch!");
-			System.out.println(e.getMessage());
-			highscore = new Highscore();
-		}
-		return highscore;
+//		String jsonText = "";
+//		Highscore highscore;
+//		try {
+//			FileReader fReader = new FileReader("highscore.json");
+//			try(BufferedReader reader = new BufferedReader(fReader)) {
+//				System.out.println(jsonText);
+//				highscore = json.fromJson(Highscore.class, reader);
+//			}
+//		} catch (Exception e) {
+//			System.out.println("Outer Catch!");
+//			System.out.println(e.getMessage());
+//			highscore = new Highscore();
+//		}
+		return new Highscore();
 	}
 }
